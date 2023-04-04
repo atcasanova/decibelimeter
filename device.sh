1cf274d05cd7de9ff85d2079f15ee9a5741dea9b
Alfredo Casanova <atcasanova@gmail.com>
2023-04-04 16:46:37 -0300

    Create device.sh

---
+++ device.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+initialize_device() {
+  local vendor_id="$1"
+  local product_id="$2"
+  hid_device_path=$(find /sys/bus/hid/devices -maxdepth 1 -name "*:${vendor_id}:${product_id}.*" -type d | head -n 1)
+  if [[ -z "$hid_device_path" ]]; then
+    echo "Device not found. Please ensure it's connected."
+    exit 1
+  fi
+  hidraw_path=$(find "$hid_device_path" -name "hidraw*" -type d | head -n 1)
+  device_path="/dev/$(basename "$hidraw_path")"
+  echo "$device_path"
+}
+
+read_data_from_device() {
+  local device_path="$1"
+  local message_length=16
+  local message=$(dd if="$device_path" bs=1 count=$message_length 2>/dev/null | xxd -p -c $message_length)
+  echo "$message"
+}
+
+close_device() {
+  # As the device is not being held open by the script, there is no need to close it explicitly.
+  # The device will be automatically closed when the script exits.
+  true
+}
