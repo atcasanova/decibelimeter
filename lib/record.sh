feee1722558c011580a23fcbccb3538b1f5fc07b
Alfredo Casanova <atcasanova@gmail.com>
2023-04-04 16:34:56 -0300

    Update record.sh

--- lib/record.sh
+++ lib/record.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-declare -f print_value
-declare -f assign_spl
-
-function assign_spl(){
-	message="$1"
-	echo "obase=10; ibase=16; scale=1; ${message:0:3}"|bc|sed 's/^../\0./g'
+# Function to assign SPL based on the message received
+assign_spl() {
+  local message="$1"
+  spl=$(echo "scale=1; ${message:0:4} / 10" | bc)
 }
 
-function print_value(){
-	valor="$1"
-	output="$(date "+%Y-%m-%d %H:%M:%S")"
-	output+='	'
-	output+="SPL: $(assign_spl "$valor")"
-	echo "$output"
+# Function to format the record
+print_record() {
+  local output
+  output=$(date +"%H:%M:%S")
+  output+="\t SPL: ${spl}dB${filter^^}"
+  output+=$([[ $max_mode == 1 ]] && echo -e "\t MAX mode")
+  echo "$output"
 }
 
+# Function to initialize a record based on the message received
+initialize_record() {
+  local message="$1"
+  assign_spl "$message"
+  assign_settings "$message"
+}
