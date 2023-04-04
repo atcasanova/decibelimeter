5f2fa9dd087434d217c365b8f43fbf64442d27f4
Alfredo Casanova <atcasanova@gmail.com>
2023-04-04 17:11:36 -0300

    Update gm1356.sh

--- gm1356.sh
+++ gm1356.sh
@@ -7,14 +7,36 @@ source device.sh
 vendor_id="64BD"
 product_id="74E3"
 
+
 device_path=$(initialize_device "$vendor_id" "$product_id")
 
+read_current_state() {
+  send_state_request "$device_path"
+  read_data_from_device "$device_path"
+}
+
+send_state_request() {
+  local device_path="$1"
+  STATE_REQUEST="b3$(head /dev/urandom | tr -dc 'a-f0-9' | head -c 6)00000000"
+  echo -n "$STATE_REQUEST" | xxd -r -p > "$device_path"
+}
+
+set_device_settings() {
+  local packed_settings=$(pack_settings)
+  data="56${packed_settings}000000000000"
+  echo -n "$data" | xxd -r -p > "$device_path"
+  read_current_state
+}
+
+# Set device settings
+set_device_settings
+
+# Read data from the device continuously
 while true; do
-  message=$(read_data_from_device "$device_path")
-  initialize_record "$message"
-  print_record
-  sleep_time=$(($speed == "fast" ? 1 : 2))
-  sleep $sleep_time
+  data=$(read_current_state)
+  assign_spl "$data"
+  echo -e "$(date +'%H:%M:%S')\t SPL: ${spl}$(format)"
+  sleep "${settings[speed]}"
 done
 
 close_device "$device_path"
